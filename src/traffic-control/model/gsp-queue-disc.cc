be8494491465163e3efb218b0fd8174b117c0566
Karthik <tarunkarthik999@gmail.com>
2018-03-19 20:31:33 +0530

    Added more parameters and made skeleton for functions

--- src/traffic-control/model/gsp-queue-disc.cc
+++ src/traffic-control/model/gsp-queue-disc.cc
@@ -1,9 +1,6 @@
 #include "ns3/log.h"
-#include "ns3/enum.h"
-#include "ns3/uinteger.h"
-#include "ns3/double.h"
-#include "ns3/simulator.h"
-#include "ns3/abort.h"
+#include "fifo-queue-disc.h"
+#include "ns3/object-factory.h"
 #include "ns3/drop-tail-queue.h"
 #include "ns3/net-device-queue-interface.h"
 
@@ -19,11 +16,12 @@ namespace ns3{
       .SetParent<DropTailQueue>()   //confirm
       .SetGroupName ("TrafficControl")
       .AddConstructor<GspQueueDisc> ()
-      .AddAttribute ("QueueLimit",
-                     "Queue limit in packets",
-                     UintegerValue (100),
-                     MakeUintegerAccessor (&GspQueueDisc::SetQueueLimit),
-                     MakeUintegerChecker<uint32_t> ()) // confirm
+      .AddAttribute ("MaxSize",
+                     "The max queue size",
+                     QueueSizeValue (QueueSize ("1000p")),
+                     MakeQueueSizeAccessor (&QueueDisc::SetMaxSize,
+                                            &QueueDisc::GetMaxSize),
+                     MakeQueueSizeChecker ()) // confirm
       .AddAttribute ("A",
                      "Value of alpha",
                      DoubleValue (2),
@@ -32,16 +30,72 @@ namespace ns3{
       .AddAttribute("Interval",
                     "Value that will be incremented to the time out",
                     TimeValue (),    //twice of RTT
-                    MakeTimeAccessor (&GspQueueDisc::interval),
+                    MakeTimeAccessor (&GspQueueDisc::SetInterval,
+                                      &GspQueueDisc::GetInterval),
                     MakeTimeChecker ())
       .AddAttribute("Adaptive Variable",
                     "Controlling the adaptation speed",
                     TimeValue (),
                     MakeTimeAccessor (&GspQueueDisc::adapt),
                     MakeTimeChecker ())
+      .AddAttribute("Threshold",
+                    "Limit above which the packet should be dropped",
+                    DoubleValue (500),
+                    MakeDoubleAccessor(&GspQueueDisc::SetThreshold,
+                                       &GspQueueDisc::GetThreshold),
+                    MakeDoubleAccessor ())
+      .AddAttribute("Timeout",
+                    "amount of time for which the packets will not be dropped",
+                    TimeValue (Seconds(0)),
+                    MakeTimeAccessor (&GspQueueDisc::SetTimeout,
+                                      &GspQueueDisc::GetTimeout),
+                    MakeTimeChecker ())
 
     ;
     return tid;
+  }
+
+  GspQueueDisc::GspQueueDisc ()
+    : FifoQueueDisc () //check parameters to be SetNetDevice
+    {
+      NS_LOG_FUNCTION (this);
+    }
+
+  GspQueueDisc::~GspQueueDisc ()
+  {
+    NS_LOG_FUNCTION (this);
+  }
+
+  bool
+  GspQueueDisc::DoEnqueue (Ptr<QueueDiscItem> item)
+  {
+    NS_LOG_FUNCTION (this << item);
+    //Enqueue function goes here
+
+  }
+
+  Ptr<QueueDiscItem>
+  GspQueueDisc::DoDequeue (void)
+  {
+    NS_LOG_FUNCTION (this);
+  }
+
+  Ptr<const QueueDiscItem>
+  GspQueueDisc::DoPeek (void)
+  {
+    NS_LOG_FUNCTION (this);
+  }
+
+  bool
+  GspQueueDisc::CheckConfig (void)
+  {
+    NS_LOG_FUNCTION (this);
+  }
+
+  void
+  GspQueueDisc::InitializeParams (void)
+  {
+    NS_LOG_FUNCTION (this);
   }
 
 }
