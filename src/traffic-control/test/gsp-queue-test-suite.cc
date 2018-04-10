f3b4c2826911144778db0cd68e1e28d014d19092
Karthik <tarunkarthik999@gmail.com>
2018-04-11 02:06:02 +0530

    Created the testsuite

--- src/traffic-control/test/gsp-queue-test-suite.cc
+++ src/traffic-control/test/gsp-queue-test-suite.cc
@@ -1,54 +1,129 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * Copyright (c) 2018 NITK Surathkal
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Authors: A Tarun Karthik <tarun.karthik.97@gmail.com>
-            Amshula U S <>
-            Manoj Kumar <>
- */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
 
- /** Network topology
- *                     10Gb/s
- *                /-------------n2
- *                /
- *      10Gb/s    /
- * n0------------n1
- *                /
- *                /    10Gb/s
- *                /-------------n3
- */
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/flow-monitor-helper.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/log.h"
 
- #include "ns3/core-module.h"
- #include "ns3/network-module.h"
- #include "ns3/internet-module.h"
- #include "ns3/flow-monitor-helper.h"
- #include "ns3/point-to-point-module.h"
- #include "ns3/applications-module.h"
- #include "ns3/traffic-control-module.h"
+using namespace ns3;
 
- using namespace ns3;
+NS_LOG_COMPONENT_DEFINE ("GspTestSuite");
 
- NS_LOG_COMPONENT_DEFINE ("SfqExample");
+int
+main (int argc, char *argv[])
+{
+  Config::SetDefault ("ns3::OnOffApplication::PacketSize", UintegerValue (210));
+  Config::SetDefault ("ns3::OnOffApplication::DataRate", StringValue ("10Gb/s"));
 
- NodeContainer n0n1;
- NodeContainer n1n2;
- NodeContainer n1n3;
+  NS_LOG_INFO ("Create nodes.");
+  NodeContainer c;
+  c.Create (4);
+  NodeContainer n0n2 = NodeContainer (c.Get (0), c.Get (2));
+  NodeContainer n1n2 = NodeContainer (c.Get (1), c.Get (2));
+  NodeContainer n3n2 = NodeContainer (c.Get (3), c.Get (2));
 
- Ipv4InterfaceContainer i0i1;
- Ipv4InterfaceContainer i1i2;
- Ipv4InterfaceContainer i1i3;
+  InternetStackHelper internet;
+  internet.Install (c);
 
- uint16_t port = 50000;
+  NS_LOG_INFO ("Create channels.");
+  PointToPointHelper p2p;
+  p2p.SetDeviceAttribute ("DataRate", StringValue ("10Gbps"));
+  p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  NetDeviceContainer d0d2 = p2p.Install (n0n2);
+
+  NetDeviceContainer d1d2 = p2p.Install (n1n2);
+
+  NetDeviceContainer d3d2 = p2p.Install (n3n2);
+
+  NS_LOG_INFO ("Assign IP Addresses.");
+  Ipv4AddressHelper ipv4;
+  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer i0i2 = ipv4.Assign (d0d2);
+
+  ipv4.SetBase ("10.1.2.0", "255.255.255.0");
+  Ipv4InterfaceContainer i1i2 = ipv4.Assign (d1d2);
+
+  ipv4.SetBase ("10.1.3.0", "255.255.255.0");
+  Ipv4InterfaceContainer i3i2 = ipv4.Assign (d3d2);
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  NS_LOG_INFO ("Create Applications.");
+  uint16_t port = 9;
+
+  OnOffHelper onoff ("ns3::UdpSocketFactory",
+                     Address (InetSocketAddress (i3i2.GetAddress (0), port)));
+  onoff.SetConstantRate (DataRate ("10Gb/s"));
+
+  ApplicationContainer apps = onoff.Install (c.Get (0));
+  apps.Start (Seconds (1.0));
+  apps.Stop (Seconds (10.0));
+
+  // Create a packet sink to receive these packets
+  PacketSinkHelper sink ("ns3::UdpSocketFactory",
+                         Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
+  apps = sink.Install (c.Get (3));
+  apps.Start (Seconds (1.0));
+  apps.Stop (Seconds (10.0));
+
+  // Create a similar flow from n3 to n1, starting at time 1.1 seconds
+  onoff.SetAttribute ("Remote",
+                      AddressValue (InetSocketAddress (i1i2.GetAddress (0), port)));
+  apps = onoff.Install (c.Get (3));
+  apps.Start (Seconds (1.1));
+  apps.Stop (Seconds (10.0));
+
+  // Create a packet sink to receive these packets
+  apps = sink.Install (c.Get (1));
+  apps.Start (Seconds (1.1));
+  apps.Stop (Seconds (10.0));
+
+  onoff.SetAttribute ("Remote",
+                      AddressValue (InetSocketAddress (i0i2.GetAddress (0), port)));
+  apps = onoff.Install (c.Get (3));
+  apps.Start (Seconds (1.1));
+  apps.Stop (Seconds (10.0));
+
+  // Create a packet sink to receive these packets
+  apps = sink.Install (c.Get (0));
+  apps.Start (Seconds (1.1));
+  apps.Stop (Seconds (10.0));
+
+  onoff.SetAttribute ("Remote",
+                      AddressValue (InetSocketAddress (i3i2.GetAddress (0), port)));
+  apps = onoff.Install (c.Get (0));
+  apps.Start (Seconds (1.1));
+  apps.Stop (Seconds (10.0));
+
+  // Create a packet sink to receive these packets
+  apps = sink.Install (c.Get (3));
+  apps.Start (Seconds (1.1));
+  apps.Stop (Seconds (10.0));
+
+  FlowMonitorHelper flowmonHelper;
+  if (enableFlowMonitor)
+    {
+      flowmonHelper.InstallAll ();
+    }
+
+  NS_LOG_INFO ("Run Simulation.");
+  Simulator::Stop (Seconds (11));
+  Simulator::Run ();
+  NS_LOG_INFO ("Done.");
+
+  if (enableFlowMonitor)
+    {
+      flowmonHelper.SerializeToXmlFile ("simple-global-routing.flowmon", false, false);
+    }
+
+  Simulator::Destroy ();
+  return 0;
+}
